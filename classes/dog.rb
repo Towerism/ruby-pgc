ff8b2a670d2e830790ec343347210a798c254625
Martin Fracker <martin.frackerjr@gmail.com>
2017-10-10 20:48:06 -0500

    Step 5

--- classes/dog.rb
+++ classes/dog.rb
@@ -1,5 +1,15 @@
 class Dog
-  attr_accessor :name, :age
+  attr_reader :name, :age
+
+  def name=(value)
+    raise "Name can't be blank!" if value == ''
+    @name = value
+  end
+
+  def age=(value)
+    raise "An age of #{value} isn't valid!" if value < 0
+    @age = value
+  end
 
   def talk
     puts 'Bark!'
@@ -13,3 +23,10 @@ class Dog
     puts "#{@name} is #{@age} years old."
   end
 end
+
+dog = Dog.new
+dog.name = 'Daisy'
+dog.age = 3
+dog.report_age
+dog.talk
+dog.move('bed')
