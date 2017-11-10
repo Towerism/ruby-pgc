58055fa31126213db3c1864517e6563cb85db9cf
Martin Fracker <martin.frackerjr@gmail.com>
2017-11-09 22:32:54 -0600

    step 4

--- initialize-instances/employees.rb
+++ initialize-instances/employees.rb
@@ -1,9 +1,8 @@
 class Employee
   attr_reader :name, :salary
 
-  def initialize(name = 'Anonymous', salary = 0.0)
+  def initialize(name = 'Anonymous')
     self.name = name
-    self.salary = salary
   end
 
   def name=(name)
@@ -11,6 +10,15 @@ class Employee
       raise "Name can't be blank!"
     end
     @name = name
+  end
+end
+
+class SalariedEmployee < Employee
+  attr_reader :salary
+
+  def initialize(name = 'Anonymous', salary = 0.0)
+    super(name)
+    self.salary = salary
   end
 
   def salary=(salary)
@@ -21,8 +29,39 @@ class Employee
   end
 
   def print_pay_stub
-    puts "Name: #{@name}"
-    pay_for_period = (@salary / 365.0) * 14
+    puts "Name: #{name}"
+    pay_for_period = (salary / 365.0) * 14
+    formatted_pay = format("%.2f", pay_for_period)
+    puts "Pay This Period: $#{formatted_pay}"
+  end
+end
+
+class HourlyEmployee < Employee
+  attr_reader :hourly_wage, :hours_per_week
+
+  def initialize(name = 'Anonymous', hourly_wage = 0.0, hours_per_week = 0.0)
+    super(name)
+    self.hourly_wage = hourly_wage
+    self.hours_per_week = hours_per_week
+  end
+
+  def hourly_wage=(hourly_wage)
+    if hourly_wage < 0
+      raise "An hourly wage of #{salary} isn't valid"
+    end
+    @hourly_wage = hourly_wage
+  end
+
+  def hours_per_week=(hours_per_week)
+    if hours_per_week < 0
+      raise "A work load of #{hours_per_week} hours per week isn't valid"
+    end
+    @hours_per_week = hours_per_week
+  end
+
+  def print_pay_stub
+    puts "Name: #{name}"
+    pay_for_period = hourly_wage * hours_per_week * 2
     formatted_pay = format("%.2f", pay_for_period)
     puts "Pay This Period: $#{formatted_pay}"
   end
