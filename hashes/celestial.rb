7e341eb084e3a871aabdc1f9e26ab11cae432731
Martin Fracker <martin.frackerjr@gmail.com>
2017-12-07 21:27:57 -0600

    step 3

--- hashes/celestial.rb
+++ hashes/celestial.rb
@@ -2,9 +2,12 @@ class CelestialBody
   attr_accessor :type, :name
 end
 
-default_body = CelestialBody.new
-default_body.type = 'planet'
-bodies = Hash.new(default_body)
+bodies = Hash.new do |hash, key|
+  body = CelestialBody.new
+  body.type = 'planet'
+  hash[key] = body
+  body
+end
 
 bodies['Mars'].name = 'Mars'
 
