9299a6eba6aa09b2f6e68715a48f85006b273767
Martin Fracker <martin.frackerjr@gmail.com>
2017-12-07 20:54:54 -0600

    Add ch7 exercise

---
+++ hashes/candidate.rb
@@ -0,0 +1,23 @@
+class Candidate
+  attr_accessor :name, :age, :occupation, :hobby, :birthplace
+
+  def initialize(name, age: nil, occupation: nil, hobby: nil, birthplace:)
+    self.name = name
+    self.age = age
+    self.occupation = occupation
+    self.hobby = hobby
+    self.birthplace = birthplace
+  end
+end
+
+def print_summary(candidate)
+  puts "Candidate: #{candidate.name}"
+  puts "Age: #{candidate.age}"
+  puts "Occupation: #{candidate.occupation}"
+  puts "Hobby: #{candidate.hobby}"
+  puts "Birthplace: #{candidate.birthplace}"
+end
+
+candidate = Candidate.new("Carl Barnes", 
+  age: 49, occupation: "Attorney")
+print_summary(candidate)
