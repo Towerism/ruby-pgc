128524d87005ff4776d5a340178473c4464c7ca4
Martin Fracker <martin.frackerjr@gmail.com>
2017-12-07 21:26:27 -0600

    step 2

--- hashes/celestial.rb
+++ hashes/celestial.rb
@@ -7,4 +7,12 @@ default_body.type = 'planet'
 bodies = Hash.new(default_body)
 
 bodies['Mars'].name = 'Mars'
+
+bodies['Europa'].name = 'Europa'
+bodies['Europa'].type = 'moon'
+
+bodies['Venus'].name = 'Venus'
+
+p bodies['Venus']
 p bodies['Mars']
+p bodies['Europa']
