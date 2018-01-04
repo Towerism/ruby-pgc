067594d919e0641172ce604da6eecf58b4c34306
Martin Fracker <martin.frackerjr@gmail.com>
2018-01-03 19:40:43 -0600

    Add initialize method

--- documentation/word_splitter.rb
+++ documentation/word_splitter.rb
@@ -6,8 +6,14 @@ class WordSplitter
   # The string to split into words.
   attr_accessor :string
 
+  # Creates a new instance with its string attribute
+  # set to the given string
+  def initialize(string)
+    self.string = string
+  end
+
   # Passes each word in the string to a block, one
-  # at a ime
+  # at a time
   def each
     string.split(' ').each do |word|
       yield word
