3aef77e3d52f21a15030b65fb57780ad4ee18858
Martin Fracker <martin.frackerjr@gmail.com>
2018-01-03 19:50:53 -0600

    Add simple small_oven class

---
+++ exceptions/small_oven.rb
@@ -0,0 +1,29 @@
+class SmallOven
+  attr_accessor :contents
+
+  def turn_on
+    puts "Turning oven on."
+    @state = "on"
+  end
+
+  def turn_off
+    puts "Turning oven off."
+    @state = "off"
+  end
+
+  def bake
+    unless @state == "on"
+      raise "You need to turn the oven on first!"
+    end
+
+    if @contents == nil
+      raise "There's nothing in the oven!"
+    end
+    "golden-brown #{contents}"
+  end
+end
+
+oven = SmallOven.new
+oven.turn_on
+oven.contents = 'turkey'
+puts "Serving #{oven.bake}."
